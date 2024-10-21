edaa444d777dc162e181e88f1b520f597d621435
Philo-LEE <tlgus7777@naver.com>
2024-10-21 09:31:07 +0900

    list기능 디벨롭. - 삭제한 리스트요소 순회할 때 생기는 버그 수정.

--- src/main/java/console/Console.java
+++ src/main/java/console/Console.java
@@ -122,7 +122,7 @@ public class Console {
     }
 
     //Hash맵 생성기(Board만들기)
-    static
+//    static
 
     //게시글 add 구현
     public String[] addPost(){
@@ -226,22 +226,33 @@ public class Console {
         return tempPost;
     }
 
-    //board list 구현
+    //board list 구현ㅋㅋㅋㅋㅋ
     static void postList(HashMap<Integer, String[]> postBoard){
 
         System.out.println("총 게시글은 "+ postBoard.size() +"개 입니다.\n");
 
         //post Board의 키셋 받고 정렬 후 for문 돌기
+        //인덱스 담을 배열 선언.
         Integer[] indexSet  = new Integer[postBoard.size()];
+
+        // indexSet에 차례대로 담겨야 하므로 j 선언,(for -each)에서는 j가 추가선언이 되지 않는다.ㅠ
+        int j=0;
+
+        //i에 posrtBoard의 KeySet이 담긴다.(HashMap은 비순서성)
         for(Integer i : postBoard.keySet()){
-            indexSet[i] = i;
+            //indexSet 배열이 만들어진다.(빈 값은 알아서 안들어온다)
+            indexSet[j++] = i;
         }
 
+        // indexSet을 오름차순으로 정렬한다.
+        // postBoard의 Index는 오름차순으로 삭제한거 제외하고 담긴다.
+        Arrays.sort(indexSet);
 
+        // indexSet의 인덱스를 차례대로 순회. 삭제한 것은 자동으로 넘어간다.
+        for(Integer i : indexSet){
 
-        for(Integer i = 0;i< postBoard.size();i++){
+            // tempPost의 요소를 출력
             String[] tempPost = postBoard.get(i);
-
 
             System.out.print("게시물 번호 : " + tempPost[0] +"\n작성일 : " + tempPost[4] + "\n수정일 : " + tempPost[5] + "\n제목 : " + tempPost[1] + "\n내용 : " + tempPost[2] + "\n\n");
 
